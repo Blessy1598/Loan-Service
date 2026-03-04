bd764ec06d91cb97c3ec9917d9475852b7af05c0
Blessy Babu <blessy.babu@LTMAC-015.local>
2026-03-04 10:46:11 +0530

    Getters and setter methods

--- src/main/java/com/example/loan_service/domain/LoanApplication.java
+++ src/main/java/com/example/loan_service/domain/LoanApplication.java
@@ -36,4 +36,108 @@ public class LoanApplication {
     private BigDecimal interestRate;
     private BigDecimal emi;
     private BigDecimal totalPayable;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getApplicantName() {
+        return applicantName;
+    }
+
+    public void setApplicantName(String applicantName) {
+        this.applicantName = applicantName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BigDecimal getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(BigDecimal monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public EmploymentType getEmploymentType() {
+        return employmentType;
+    }
+
+    public void setEmploymentType(EmploymentType employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public BigDecimal getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(BigDecimal loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public int getTenureMonths() {
+        return tenureMonths;
+    }
+
+    public void setTenureMonths(int tenureMonths) {
+        this.tenureMonths = tenureMonths;
+    }
+
+    public ApplicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ApplicationStatus status) {
+        this.status = status;
+    }
+
+    public RiskBand getRiskBand() {
+        return riskBand;
+    }
+
+    public void setRiskBand(RiskBand riskBand) {
+        this.riskBand = riskBand;
+    }
+
+    public BigDecimal getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(BigDecimal interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public BigDecimal getEmi() {
+        return emi;
+    }
+
+    public void setEmi(BigDecimal emi) {
+        this.emi = emi;
+    }
+
+    public BigDecimal getTotalPayable() {
+        return totalPayable;
+    }
+
+    public void setTotalPayable(BigDecimal totalPayable) {
+        this.totalPayable = totalPayable;
+    }
 }
